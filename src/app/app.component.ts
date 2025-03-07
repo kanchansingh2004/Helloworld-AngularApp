fdf3e4d41e949303124e20aa511db4262373cf43
kanchansingh2004 <kannursingh2004@gmail.com>
2025-03-07 11:45:05 +0530

    [Kanchan] On clicking BridgeLabz Logo Open New Tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,13 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'cwd-Helloworld-AngularApp';
   imgUrl = "../assets/BL_logo_square_png.png"
+  url="https://www.bridgelabz.com/";
   ngOnInit() : void{
     this.title = "Hello from BridgeLabz.";
+  }
+
+  onClick(){
+    console.log("Save Button is clicked!");
+    window.open(this.url,"_blank");
   }
 }
