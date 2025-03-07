4354748940b1d09cd3f8b0f03962ee70fd8f3009
kanchansingh2004 <kannursingh2004@gmail.com>
2025-03-07 13:30:26 +0530

    [Kanchan] validate username and display the message if wrong username given

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   userName: string = '';
   imgUrl = "../assets/BL_logo_square_png.png"
   url="https://www.bridgelabz.com/";
+  errorMessage: string = '';
   ngOnInit() : void{
     this.title = "Welcome to BridgeLabz.";
   }
@@ -21,5 +22,17 @@ export class AppComponent {
   onClick(){
     console.log("Save Button is clicked!");
     window.open(this.url,"_blank");
+  }
+
+  validation(){
+    if(!this.userName){
+      this.errorMessage = "Name is Required!!";
+    }
+    else if(!/^[A-Z][a-z]{2,}[\sA-za-z]*$/.test(this.userName)){
+      this.errorMessage = "Name must start with capital and contain minimun 3 characters!!";
+    }
+    else{
+      this.errorMessage = "";
+    }
   }
 }
